3473d20d132e978e5b139a29805018fe7edddccd
matt <mathuyoung@gmail.com>
2017-05-05 11:47:59 -0400

    First commit

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,11 @@
 /**
  * Created by matt on 05/05/2017.
  */
+
+
+import static tools.loopExample.doLoop;
+
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -8,6 +13,28 @@ public class Runner {
         System.out.println("Hello World!");
         System.out.println("This was a triumph!");
 
+        addFunction2();
+
+        doLoop(4);
+
     }
 
-}
+     public static void addFunction(){
+
+         int a = 1;
+         int b = 2;
+         int c = a + b;
+         System.out.println(c);
+
+    }
+
+    public static void addFunction2(){
+
+         addFunction();
+
+    }
+
+
+    }
+
+
---
+++ src/tools/loopExample.java
@@ -0,0 +1,26 @@
+package tools;
+
+/**
+ * Created by matt on 05/05/2017.
+ */
+public class loopExample {
+
+    public static void main(String[] args){
+
+      int x = 2;
+
+      doLoop(2);
+
+    }
+
+    public static void doLoop(int x) {
+
+        for (int i = x; i < 10; i++) {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}
